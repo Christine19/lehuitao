6c7dd18134c6f83b2efa0e6f8dac2ded38aec839
Christine <837642253@qq.com>
2018-07-25 21:33:40 +0800

    路由关联首页

--- src/App.vue
+++ src/App.vue
@@ -38,9 +38,9 @@
                 <div id="menu2" class="nav-box menuhd">
                     <ul>
                         <li class="index">
-                            <a href="#" class="">
+                              <router-link to="/index">
                                 <span class="out" style="top: 0px;">首页</span>
-                            </a>
+                              </router-link> 
                         </li>
                         <li class="news">
                             <a href="#" class="">
@@ -80,6 +80,8 @@
             </div>
         </div>
     </div>
+    <!-- 路由的出口 -->
+    <router-view></router-view>
     <!-- 首页底部 -->
     <div class="footer">
       <div class="section">
--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 引入路由模块
+import VueRouter from 'vue-router'
+// 引入首页组件
+import index from './component/index.vue'
+// 使用路由中间件
+Vue.use(VueRouter);
+// 实例化
+const router = new VueRouter({
+  routes:[{
+    path:'/index',
+    component:index
+  }]
+})
+// 挂载点
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+  el:"#app",
+  router,
+ render: h=>h(App)
+})
