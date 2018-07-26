691af5677476a85869f9d360a4ea87c8e09b0e63
Christine <837642253@qq.com>
2018-07-26 19:59:55 +0800

    完成商品详情页的渲染

--- src/main.js
+++ src/main.js
@@ -7,11 +7,15 @@ import index from './component/index.vue'
 // 引入ui框架需要的模块
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+// 引入懒加载模块
+import VueLazyload from 'vue-lazyload';
 // 使用ui中间件
 Vue.use(ElementUI);
 
 // 使用路由中间件
 Vue.use(VueRouter);
+// 使用懒加载中间件
+Vue.use(VueLazyload);
 // 实例化
 const router = new VueRouter({
   routes:[
